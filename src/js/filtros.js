b84bcc76cc39656d23a42b32c8c23f541c447a26
Orlando Mendoza <ommv.17@gmail.com>
2020-07-27 01:46:10 -0400

    arreglo buscador y precarga post

--- src/js/filtros.js
+++ src/js/filtros.js
@@ -15,3 +15,30 @@ $btnClose.addEventListener('click', () => {
     $mainFiltros.style.transform = 'translateX(-100%)'
   }
 })
+
+// Clases active de los botones del buscador
+
+const $botonesContainer = document.getElementById('botones-container')
+
+
+const eventListenerToggle = (boton1, boton2) => {
+
+  boton1.addEventListener('click', () =>{
+  
+    if (!boton1.classList.contains('active') && 
+         boton2.classList.contains('active')) {
+  
+      boton1.classList.toggle('active')
+      boton2.classList.toggle('active')
+  
+    }
+  })
+
+}
+
+eventListenerToggle($botonesContainer.children[0],
+                    $botonesContainer.children[1])
+
+eventListenerToggle($botonesContainer.children[1],
+                    $botonesContainer.children[0])
+
