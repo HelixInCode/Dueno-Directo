b8bbab9ddc660b6b1fd99e700635ee8163837ada
OrlandoMendoza17 <ommv.17@gmail.com>
2020-09-01 00:43:00 -0400

    modificacion de hamburger.js

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,33 +1,46 @@
-const $menuItems = document.getElementsByClassName('menu-items')[0]
-const $menuOverlay = document.getElementsByClassName('menu-overlay')[0]
-
+// Main Menu Elements
 const $hamburger = document.getElementById('hamburger')
+const $menuOverlay = document.getElementsByClassName('menu-overlay')[0]
+const $menuItems = document.getElementsByClassName('menu-items')[0]
+// Menu User Panel
+const $user = document.getElementsByClassName('user')[0]
+const $userPanel = $user.getElementsByTagName('ul')[0]
+const $userArrow = $user.getElementsByClassName('fa-angle-down')[0]
 
-$hamburger.addEventListener('click', ()=>{
-  if($menuItems.classList.contains('hide')){
-
-    $menuItems.classList.remove('hide')
-    $menuOverlay.classList.remove('hide')
-    
-  }else if(!$menuItems.classList.contains('hide')){
-    
-    $menuItems.classList.add('hide')
-    $menuOverlay.classList.add('hide')
+class Menu{
+  constructor(displayButton, menuList, optionalElement, class1, class2){
+    this.boton = displayButton;
+    this.menu = menuList;
+    this.option = optionalElement;
+    this.clase1 = class1;
+    this.clase2 = class2;
   }
-})
-$menuOverlay.addEventListener('click', ()=>{
-  if(!$menuItems.classList.contains('hide')){
-    
-    $menuItems.classList.add('hide')
-    $menuOverlay.classList.add('hide')
-  }
-})
+}
 
-if(document.getElementsByClassName('user')[0]){
-  const $user = document.getElementsByClassName('user')[0]
-  const $userPanel = $user.getElementsByTagName('ul')[0]
-  $user.addEventListener('click', ()=>{
-    $userPanel.classList.toggle('hide')
-    $user.getElementsByClassName('fa-angle-down')[0].classList.toggle('rotate')
+const displayMenu = (menuType) => {
+  menuType.boton.addEventListener('click', () => {
+    menuType.menu.classList.toggle(menuType.clase1)
+    menuType.option.classList.toggle(menuType.clase2)
   })
 }
+
+const menuHamburger = new Menu($hamburger, $menuItems, $menuOverlay, 'hide', 'hide')
+const menuOverlay = new Menu($menuOverlay, $menuItems, $menuOverlay, 'hide', 'hide')
+const menuUser = new Menu($user, $userPanel, $userArrow, 'hide', 'rotate')
+
+const menu = [menuHamburger, menuOverlay, menuUser]
+
+menu.forEach(menuType => displayMenu(menuType))
+
+window.addEventListener('load', (event) =>{
+  
+  if($userPanel.children[0].children[0].innerText === ""){
+    //Hay sesión iniciada
+    $user.classList.toggle('d-none')
+    
+  }else{
+    //No hay sesión iniciada
+    $menuItems.classList.toggle('four-buttons')
+    $menuItems.children[3].classList.toggle('d-none')
+  }
+})
