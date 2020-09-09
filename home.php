2baff7dc55f587f3d5700bd05eea5e4387db4282
Cin Sidan <cinthiaesidan_94@hotmail.com>
2020-09-09 20:58:35 -0300

    envio de id junto con imagen para facilitar el guardado

--- home.php
+++ home.php
@@ -135,6 +135,7 @@ if (isset($_SESSION['id'])) {
                     <form class="main-container p-3" action="process/img_user.php" method="POST" enctype="multipart/form-data">
 
                         <div class="input-container py-5">
+                            <input name="id" type="hidden" value="<?php echo $consuldates['id']; ?>">
                             <input name="imagen" type="file" accept="image/*">
                         </div>
 
