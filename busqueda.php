7b2c718293c465654ab0c32bca7308d4bd846a38
Cinthia Sidan <cinthiaesidan_94@hotmail.com>
2021-01-13 15:45:34 -0300

    Busqueda de propiedades y configuracion de formulario main

---
+++ busqueda.php
@@ -0,0 +1,128 @@
+<?php
+include('conexion.php');
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Dueño Directo</title>
+
+  <!-- icono de la pestaña -->
+  <link rel="icon" href="dist/img/logo-icon.png">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Google Fonts Ubuntu bold-->
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="dist/css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <!-- <link rel="stylesheet" href="src/css/style.css"> -->
+  <link rel="stylesheet" href="src/css/style.css">
+  <link rel="stylesheet" href="src/css/servicios.css">
+</head>
+
+<body>
+
+
+
+<footer id="footer">
+
+    <div class="contenedor-central py-4">
+
+      <div class="redes">
+        <h5>¡Seguínos en nuestras redes!</h5>
+
+        <div class="item-redes">
+          <img src="./dist/img/icons/whatsapp.svg" alt="">
+          <a href="">+54 9 123 4567</a>
+        </div>
+
+        <div class="item-redes">
+          <img src="./dist/img/icons/instagram.svg" alt="">
+          <a href="">Instagram/</a>
+        </div>
+
+        <div class="item-redes">
+          <img src="./dist/img/icons/facebook.svg" alt="">
+          <a href="">Facebook/</a>
+        </div>
+      </div>
+
+      <div class="enlaces">
+        <h5>Publicaciones Destacadas</h5>
+
+        <ul class="items-enlaces">
+          <li>
+            <a href="#?">Mendoza</a>
+          </li>
+          <li>
+            <a href="#?">San Luis</a>
+          </li>
+          <li>
+            <a href="#?">Buenos Aires</a>
+          </li>
+          <li>
+            <a href="#?">Córdoba</a>
+          </li>
+        </ul>
+
+        <ul class="items-enlaces">
+          <li>
+            <a href="#?">Tucumán</a>
+          </li>
+          <li>
+            <a href="#?">Neuquén</a>
+          </li>
+          <li>
+            <a href="#?">Salta</a>
+          </li>
+          <li>
+            <a href="#?">Misiones</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="img-container">
+        <img src="dist/img/DD-LOGO.png" alt="">
+      </div>
+
+    </div>
+    <div class="contenedor-inferior">
+      © 2020 Copyright:
+      <a href="#"> dueñosdirecto.com</a>
+    </div>
+  </footer>
+  <!-- jQuery -->
+  <script type="text/javascript" src="dist/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="dist/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="dist/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="dist/js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript" src="src/js/svg.js"></script>
+  <!-- <script type="text/javascript" src="src/js/fetch.js"></script> -->
+  <script type="text/javascript" src="src/js/hideShowModals.js"></script>
+  <script type="text/javascript" src="src/js/loginValidation.js"></script>
+  <script type="text/javascript" src="src/js/filtros.js"></script>
+  <script type="text/javascript" src="src/js/filtrosValidation.js"></script>
+  <script type="text/javascript" src="src/js/hamburger.js"></script>
+  <!-- Llamado a las publicaciones destacadas -->
+  <!-- <script type="text/javascript" src="src/js/fetchFeatured.js"></script> -->
+  <!-- <script type="text/javascript">
+    modalError('Mensaje de prueba');
+  </script> -->
+  <!-- <script type="text/javascript" src="src/js/modalMessageSentAppears.js"></script> -->
+</body>
+
+</html>
+
+<!-- Imagen de <a href="https://pixabay.com/es/users/PublicDomainPictures-14/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2257">PublicDomainPictures</a> en <a href="https://pixabay.com/es/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2257">Pixabay</a> -->
